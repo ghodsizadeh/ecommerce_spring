a2a5bb54fe805f99db895c0a5179e6863c0d4af6
Mehdi Ghodsizadeh <mehdi.ghodsizadeh@gmail.com>
2022-03-07 17:07:42 +0330

    First Commit
    add spring initializer

---
+++ src/test/java/com/chandkhat/ecommerce/web/WebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chandkhat.ecommerce.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
