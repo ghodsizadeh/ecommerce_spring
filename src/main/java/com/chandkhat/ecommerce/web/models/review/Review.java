48594f123e2a6cce8883ca9a38423c7113096393
Mehdi Ghodsizadeh <mehdi.ghodsizadeh@gmail.com>
2022-03-07 18:38:10 +0330

    Add Review
    - Entity
    - Repository

---
+++ src/main/java/com/chandkhat/ecommerce/web/models/review/Review.java
@@ -0,0 +1,33 @@
+package com.chandkhat.ecommerce.web.models.review;
+
+import com.chandkhat.ecommerce.web.models.AbstractEntity;
+import com.sun.istack.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = false)
+@Entity
+@Table(name = "review")
+public class Review extends AbstractEntity {
+
+    @NotNull
+    @Column(name = "text", nullable = false)
+    private String title;
+
+    @NotNull
+    @Column(name = "description", nullable = false)
+    private String description;
+
+    @NotNull
+    @Column(name = "rating", nullable = false)
+    private Long rating;
+}
---
+++ src/main/java/com/chandkhat/ecommerce/web/models/review/ReviewRepository.java
@@ -0,0 +1,6 @@
+package com.chandkhat.ecommerce.web.models.review;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+}
